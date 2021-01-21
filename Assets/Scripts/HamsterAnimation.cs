0150a85f4fc2ad6c35dc5bc798a3c6b8e6239a9a
DESKTOP-M20DH03\jdleroy <jdleroy@wisc.edu>
2021-01-21 13:21:36 -0600

    Adding basic movement and animations.

---
+++ Assets/Scripts/HamsterAnimation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class HamsterAnimation : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite sprite;
+
+    private bool facingRight = false;
+
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            if (facingRight)
+            {
+                spriteRenderer.flipX = false;
+            }
+
+            facingRight = false;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            if (!facingRight)
+            {
+                spriteRenderer.flipX = true;
+            }
+
+            facingRight = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float movementForce = 1000f;
+    public float jumpForce = 10000f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d"))
+        {
+            Vector2 vForce = new Vector2(movementForce * Time.deltaTime, 0);
+            rb.AddForce(vForce);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            Vector2 vForce = new Vector2(-movementForce * Time.deltaTime, 0);
+            rb.AddForce(vForce);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (Input.GetKey("space"))
+        {
+            Vector2 vForce = new Vector2(0, jumpForce * Time.deltaTime);
+            rb.AddForce(vForce);
+        }
+    }
+}
