e4306954268b36384eeead5eee6e2e610fe6356b
DESKTOP-M20DH03\jdleroy <jdleroy@wisc.edu>
2021-01-21 20:09:30 -0600

    Adding a proper fade out from the main menu.

--- Assets/Scripts/click_single_player.cs
+++ Assets/Scripts/click_single_player.cs
@@ -3,11 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class click_single_player : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
+    public GameObject gameObject;
+    private bool clicked = false;
+    private int frames = 0;
+    private Color color;
 
     // Loads Single Player
     void OnMouseDown()
     {
-        SceneManager.LoadScene("Playground");
+        clicked = true;
+        color = gameObject.GetComponent<Renderer>().material.color;
+    }
+
+    // Fades to black
+    void Update()
+    {
+        if (clicked)
+        {
+            color.a += 0.001f;
+            gameObject.GetComponent<Renderer>().material.color = color;
+
+            frames++;
+            if (frames >= 1000)
+                SceneManager.LoadScene("Playground");
+        }
     }
 }
