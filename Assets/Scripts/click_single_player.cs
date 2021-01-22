740785cc40feda3e380c31dce237940625f9ec33
DESKTOP-M20DH03\jdleroy <jdleroy@wisc.edu>
2021-01-21 19:35:53 -0600

    Adding a basic main menu.

---
+++ Assets/Scripts/click_single_player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class click_single_player : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    // Loads Single Player
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("Playground");
+    }
+}
