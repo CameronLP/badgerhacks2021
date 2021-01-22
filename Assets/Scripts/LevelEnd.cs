40ed855ac606531e5f6729758258d563b6a08a5a
DESKTOP-M20DH03\jdleroy <jdleroy@wisc.edu>
2021-01-22 13:45:43 -0600

    Adding a win condition and tidying up some of the fade in/fade out of menus.

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour
+{
+    public new GameObject gameObject;   // Hamster
+
+    public GameObject fade_menu;        // Fade Menu
+
+    private bool completed = false;
+    private int frames = 0;
+    private Color color;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        completed = true;
+    }
+
+    // Fades to black
+    void FixedUpdate()
+    {
+        if (completed)
+        {
+            color.a += 0.01f;
+            fade_menu.GetComponent<Renderer>().material.color = color;
+
+            frames++;
+            if (frames >= 100)
+                SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
