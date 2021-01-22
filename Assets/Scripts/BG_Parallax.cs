397fee8e7879210fbb478d3af054ed14d59ae2fc
Cameron <penne@wisc.edu>
2021-01-21 22:29:43 -0600

    Add space background parallax

--- Assets/Scripts/BG_Parallax.cs
+++ Assets/Scripts/BG_Parallax.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class BG_Parallax : MonoBehaviour
 {
-	private float length, startpos;
+	private float xlength, ylength, xstartpos, ystartpos;
 		public GameObject cam;
 		public float parallaxEffect;
 	
     // Start is called before the first frame update
     void Start()
     {
-        startpos = transform.position.x;
-		length = GetComponent<SpriteRenderer>().bounds.size.x;
+        xstartpos = transform.position.x;
+		ystartpos = transform.position.y;
+		xlength = GetComponent<SpriteRenderer>().bounds.size.x;
+		ylength = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float dist = (cam.transform.position.x * parallaxEffect);
-		transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        float xdist = (cam.transform.position.x * parallaxEffect);
+		float ydist = (cam.transform.position.y * parallaxEffect);
+		transform.position = new Vector3(xstartpos + xdist, ystartpos + ydist, transform.position.z);
     }
 }
